476eca31c25257cb43be948449577d5319c241ff
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2019-03-23 00:58:07 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,36 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+radio.setGroup(12)
+RamieRobota.WyswietlIkone(IconNames.Happy)
+basic.showIcon(IconNames.Happy)
+RamieRobota.InicjujRamieRobota()
+basic.pause(2000)
+RamieRobota.WyswietlIkone(IconNames.Yes)
+basic.showIcon(IconNames.Yes)
+basic.showString("G")
+RamieRobota.RamieDoGory()
+basic.pause(5000)
+basic.showString("Z")
+RamieRobota.ZamknijSzczeki()
+basic.pause(5000)
+basic.showString("L")
+RamieRobota.RamieWLewo()
+basic.pause(5000)
+basic.showString("O")
+RamieRobota.OtworzSzczeki()
+basic.pause(5000)
+basic.showString("D")
+RamieRobota.RamieNaDol()
+basic.pause(5000)
+basic.showString("Z")
+RamieRobota.ZamknijSzczeki()
+basic.pause(5000)
+basic.showString("G")
+RamieRobota.RamieDoGory()
+basic.pause(5000)
+basic.showString("P")
+RamieRobota.RamieWPrawo()
+
+
+basic.forever(function () {
+
+})
+ 
